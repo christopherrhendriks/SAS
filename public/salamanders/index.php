7f4a85dc6fb49cb11f6da0beee1b9611f94d41e0
Charlie Wallin <charliekwallin@gmail.com>
2022-03-11 12:27:51 -0500

    Starter files for phase01 are complete.

--- public/salamanders/index.php
+++ public/salamanders/index.php
@@ -1,14 +1,34 @@
-<?php require_once('../../private/initialize.php'); 
+<?php 
+  /* ------------------------------------------------------------------
+    
+  1.)
+  * Use require_once to reference the initialize.php file
+  * Use a relative file path
+
+  2.)
+
+    Enter your code in the salamander array below
+
+    Use this array element to start your array.
+    ['id' => '1', 'salamanderName' => 'Red-Legged Salamander'],
+    
+    Add these salamanders to your array
+    id 2 Pigeon Mountain Salamander
+    id 3 ZigZag Salamander'
+    id 4 Slimy Salamander
+
+    ------------------------------------------------------------------ */
+
 
 $salamanders = [
-  ['id' => '1',  'salamanderName' => 'Red-Legged Salamander'],
-  ['id' => '2',  'salamanderName' => 'Pigeon Mountain Salamander'],
-  ['id' => '3',  'salamanderName' => 'ZigZag Salamander'],
-  ['id' => '4',  'salamanderName' => 'Slimy Salamander'],
 ];
 
-$page_title = 'Salamanders'; 
-include(SHARED_PATH . '/salamander-header.php'); 
+$page_title = 'Salamanders';
+
+/* ------------------------------------------------------------------
+Use require_once with the SHARED_PATH constant to reference 
+the salamander-header.php file
+------------------------------------------------------------------ */
 
 ?>
 
@@ -27,8 +47,19 @@ include(SHARED_PATH . '/salamander-header.php');
 
       <?php foreach($salamanders as $salamander) { ?>
         <tr>
-          <td><?= h($salamander['id']); ?></td>
-    	    <td><?= h($salamander['salamanderName']); ?></td>
+          <!------------------------------------------------------------------
+        Your code here
+        Delete this line.
+
+        Add the table data for the
+        row 1: salamander id
+        row 2: salamander name
+        rows 3, 4, and 5 are already done for you.
+        
+        You will need to reference the salamander id and name from the salamanders array
+      ------------------------------------------------------------------>
+          <td><!-- row 1 --></td>
+    	    <td><!-- row 2 --></td>
           <td><a class="action" href="<?= url_for('salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
           <td><a class="action" href="">Edit</a></td>
           <td><a class="action" href="">Delete</a></td>

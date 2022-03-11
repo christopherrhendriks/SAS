3ebd044ffc6176fa78fca39afe08082242dc49ad
Charlie Wallin <charliekwallin@gmail.com>
2022-03-11 11:33:09 -0500

    Quick fix. Code is ready for starter-files -again

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
-<?php require_once('../private/initialize.php'); ?>
-
 <?php 
+require_once('../private/initialize.php'); 
 $page_title = 'Home';
-include(SHARED_PATH . '/salamander-header.php'); 
+include_once(SHARED_PATH . '/salamander-header.php'); 
 ?>
 <h1>Welcome to SAS</h1>
 <p>The Southern Appalachian Mountains Region is often hailed as the Salamander Capital of the World. In fact, the Smithsonian Conservation Biology Institute proclaims that the Appalachian region is home to more salamander species than anywhere else in the world, making it a true hotspot for salamander biodiversity.</p> 

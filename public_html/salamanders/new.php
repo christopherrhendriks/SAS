0f226b5eb5707b5d0febe270a4c087c2f82c8be5
Chris <christopherrhendriks@students.abtech.edu>
2022-05-05 13:44:06 -0400

    Adding function for headers and error codes

--- public_html/salamanders/new.php
+++ public_html/salamanders/new.php
@@ -1,1 +1,18 @@
-<?php require_once('../../private/initialize.php');
+<?php 
+
+require_once('../../private/initialize.php');
+
+$test = $_GET['test'] ?? '';
+if($test == '404') {
+  error_404();
+}
+elseif($test == '500') {
+  error_500();
+}
+elseif($test == 'redirect') {
+  redirect_to(url_for('salamanders/index.php'));
+}
+else {
+  echo('No error');
+}
+?>
